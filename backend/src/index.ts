f353dcb62690cc3cef47d716aaeeb5deb434aa94
Eldon Williams <eldon.ew.com@gmail.com>
2022-09-19 07:58:14 -0500

    very basic web server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Backend");
+import { config, } from "dotenv"; config();
+
+import express from "express";
+
+const app = express();
+
+app.get('', (_, res) => {
+    res.send({ "Hello": "World", });
+});
+
+app.listen(process.env.PORT ?? 9000, () => {
+    console.log(`Server is up on port ${process.env.PORT ?? 9000}`);
+});
