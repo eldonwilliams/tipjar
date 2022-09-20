94233d66bc95874a702e1c5980455057ad69d945
Eldon Williams <eldon.ew.com@gmail.com>
2022-09-20 08:48:18 -0500

    added handler loading & cors/cookie middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,30 @@
 import { config, } from "dotenv"; config();
 
 import express from "express";
+import cors from "cors";
+import cookies from "cookie-parser";
+import { readdir, } from "fs";
+import { join, } from "path";
+import { contains } from "./shared/arrayutil";
+import { Handler } from "./global";
 
 const app = express();
+app.use(cors({
+    credentials: true,
+}));
+app.use(cookies());
 
-app.get('', (_, res) => {
-    res.send({ "Hello": "World", });
+readdir(join(__dirname, 'handlers'), { withFileTypes: true, }, (err, files) => {
+    if (err) console.log(err);
+    files.filter(f => contains(["ts", "js"], f.name.split('.').pop())).forEach(file => {
+        console.log(`Loading Handler ${file.name}`)
+        const fileModule: { default: Handler, } = require(join(__dirname, 'handlers', file.name));
+        if (typeof fileModule.default !== "function") return;
+        fileModule.default({ application: app, });
+    });
 });
 
 app.listen(process.env.PORT ?? 9000, () => {
     console.log(`Server is up on port ${process.env.PORT ?? 9000}`);
+    process.env.STARTED_AT = Date.now().toString();
 });
